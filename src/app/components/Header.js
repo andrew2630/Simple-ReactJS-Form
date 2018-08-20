1cb70012c87eb19ece5dea4ccb1aad005b2d53bf
andrew2630 <kontakt@andrzejpudzisz.com>
2018-08-20 12:04:19 +0200

    #1

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,7 +5,7 @@ export class Header extends React.Component {
         return (
             <header className="row header">
                     <div className="col px-0 logo"></div>
-                    <div className="col px-0 align-self-end link"><a href="https://unamo.com/">www.unamo.com</a></div>
+                    <div className="col px-0 align-self-end link"><a href="https://www.google.com/">www.fantasticpage.com</a></div>
             </header>
         );
     }
